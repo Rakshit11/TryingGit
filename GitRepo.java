28023e76d8d0505a9de5c0c5881cf839b0ad19ac
Rakshit Kashyap <3rakshitkashyap@gmail.com>
2020-08-18 22:17:50 +0530

    Git testing successful

---
+++ GitRepo.java
@@ -0,0 +1,10 @@
+package customGit;
+
+public class GitRepo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Testing my git repo ");
+	}
+
+}
---
+++ LoopTest.java
@@ -0,0 +1,12 @@
+package customGit;
+
+public class LoopTest {
+
+	public static void main(String[] args) {
+	
+		int a = 5;
+		for(int i = 0 ; i<a ;i++) {
+			System.out.println("Testing my git");
+		}
+	}
+}
