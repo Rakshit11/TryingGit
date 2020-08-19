d06d9e5140f2ced88d19e719a757af72c9fde758
Rakshit11 <3rakshitkashyap@gmail.com>
2020-08-19 17:33:52 +0530

    intital git successuful

--- LoopTest.java
+++ LoopTest.java
@@ -1,12 +1,23 @@
 package customGit;
+import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+
 
 public class LoopTest {
 
 	public static void main(String[] args) {
 	
-		int a = 5;
-		for(int i = 0 ; i<a ;i++) {
-			System.out.println("Testing my git");
+		Scanner sc =  new Scanner (System.in);
+		int n = sc.nextInt();
+		int k =  sc.nextInt();
+		int count = 0;
+		while(n-->0) {
+			int x = sc.nextInt();
+			if(x%k==0)
+				count++;
 		}
+		System.out.println(count);
+		
 	}
 }
